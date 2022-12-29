a85dc7b882e452e64e59324bd0d148fb93af1e0d
cheng <cheng-wf@hotmail.com>
2022-12-29 13:19:50 +0800

    change server url

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
     // fetch url...
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codex-famg.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
